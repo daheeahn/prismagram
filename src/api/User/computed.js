fa0569b372fd00c5d19d867d3fcde40756865e4f
daheeahn <deg9810@likelion.org>
2020-03-08 03:13:50 +0900

    #3.16 itsMe(isSelf) and amlFollowing(isFollowing) Fields part 1, 2

---
+++ src/api/User/computed.js
@@ -0,0 +1,18 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => `${parent.firstName} ${parent.lastName}`,
+    isFollowing: async ({ id: parentId }, _, { request: { user } }) => {
+      try {
+        return prisma.$exists.user({
+          AND: [{ id: parentId }, { followers_some: { id: user.id } }]
+        });
+      } catch (error) {
+        console.log("isFollowing error", error);
+        return false;
+      }
+    },
+    isSelf: ({ id: parentId }, _, { request: { user } }) => user.id === parentId
+  }
+};
--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -9,14 +9,5 @@ export default {
       const posts = await prisma.user({ id: request.user.id }).posts(); // prisma가 얼마나 깊게 들어가야 하는지 알거야!
       return { user, posts };
     }
-  },
-  User: {
-    // Normal resolver...? 느낌
-    // fullName // 여기서 타입은 custom resolver를 더할건데, 하나의 field만을 위한거야. 딱 field에만 필요한거! 다른 field에 영향 미치지 않아.
-    // 자신의 서버에서 찾으려고 할거야. prisma에서 없으면. 그게 User 지금 이 resolver겠지.
-    fullName: parent => {
-      // parent는 위에 있는 resolver
-      return `${parent.firstName} ${parent.lastName}`;
-    }
   }
 };
